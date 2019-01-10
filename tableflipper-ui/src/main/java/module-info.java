02c5505b8aabd147119e2f37ace96467c0f19ddb
Dan Langford <danlangford@ldschurch.org>
2019-01-09 23:57:53 -0700

    primarily to bug revloc02. these changes will likely not stay long

--- tableflipper-ui/src/main/java/module-info.java
+++ tableflipper-ui/src/main/java/module-info.java
@@ -4,7 +4,10 @@ module dan.langford.tableflipper.ui {
 
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
+
     requires javax.inject;
+
     requires com.google.guice;
     requires dan.langford.tableflipper;
 
