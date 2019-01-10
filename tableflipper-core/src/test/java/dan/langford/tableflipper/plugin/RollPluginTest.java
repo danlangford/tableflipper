e45572990e16df4b46d5fd119e1bb957dc8e0230
Dan Langford <danlangford@ldschurch.org>
2019-01-09 22:13:50 -0700

    latest version of Dice exposes more errors

--- tableflipper-core/src/test/java/dan/langford/tableflipper/plugin/RollPluginTest.java
+++ tableflipper-core/src/test/java/dan/langford/tableflipper/plugin/RollPluginTest.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
+import static java.lang.Integer.parseInt;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -18,19 +19,25 @@ public class RollPluginTest {
     public void testExpressions() {
         TomPlugin p = new RollPlugin();
 
-        // unfortunately invalid expressions will not always throw exceptions
-        //
-
-        List.of("78", "1d20", "2d6", "3d12+3", "4d4-4", "6d6y").forEach(expr -> {
+        // valid expressions should run well and return a valid int
+        List.of("78", "1d20", "2d6", "3d12+3", "4d4-4").forEach(expr -> {
             assertDoesNotThrow(() -> {
                 log.info("attempting expr [{}]",expr);
                 String result = p.resolve(expr).orElseThrow();
                 log.info("result of expr [{}] is [{}]", expr, result);
-                Integer.parseInt(result);
+                int x = parseInt(result);
+                log.info("result [{}] parsed to int [{}]", result, x);
+
             });
         });
 
-        assertThrows(IllegalStateException.class, () -> p.resolve("5dz"));
+        // invalid expressions should throw exceptions
+        List.of("5dz","6d6y","7d2*6", "8d5/2","9d12x3").forEach(expr -> {
+            assertThrows(IllegalStateException.class, () -> {
+                log.info("attempting expr [{}]",expr);
+                String result = p.resolve(expr).orElseThrow();
+            });
+        });
 
     }
 }
