32827ebf9187f2e4952a959e8ef359f8c599cff7
Bernardo <programming@wandrell.com>
2019-03-03 12:49:55 +0100

    Updated dice library

--- tableflipper-core/src/main/java/dan/langford/tableflipper/plugin/RollPlugin.java
+++ tableflipper-core/src/main/java/dan/langford/tableflipper/plugin/RollPlugin.java
@@ -1,18 +1,25 @@
+
 package dan.langford.tableflipper.plugin;
 
-import com.bernardomg.tabletop.dice.parser.DefaultDiceNotationExpressionParser;
-import com.bernardomg.tabletop.dice.parser.DiceNotationExpressionParser;
-import com.bernardomg.tabletop.dice.roller.DefaultRoller;
+import com.bernardomg.tabletop.dice.history.RollHistory;
+import com.bernardomg.tabletop.dice.interpreter.DiceInterpreter;
+import com.bernardomg.tabletop.dice.interpreter.DiceRoller;
+import com.bernardomg.tabletop.dice.parser.DefaultDiceParser;
+import com.bernardomg.tabletop.dice.parser.DiceParser;
 
 import java.util.Optional;
 
 public class RollPlugin implements TomPlugin {
 
-    private final DiceNotationExpressionParser diceNotation = new DefaultDiceNotationExpressionParser(new DefaultRoller());
+    private final DiceParser                   diceNotation = new DefaultDiceParser();
+
+    private final DiceInterpreter<RollHistory> roller       = new DiceRoller();
 
     @Override
-    public Optional<String> resolve(String expression, boolean verbose) {
-        Integer roll = diceNotation.parse(expression).roll();
-        return Optional.of(verbose ? expression+'='+roll : roll.toString());
+    public Optional<String> resolve(final String expression,
+            final boolean verbose) {
+        final RollHistory rolled = diceNotation.parse(expression, roller);
+        final Integer roll = rolled.getTotalRoll();
+        return Optional.of(verbose ? expression + '=' + roll : roll.toString());
     }
 }
